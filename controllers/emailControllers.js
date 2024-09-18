c7367e204c64ff4cc4b5c619c51de52750e7b27f
Akhilesh1712 <148992157+Akhilesh1712@users.noreply.github.com>
2024-09-18 23:55:11 +0530

    first change4es1t

--- controllers/emailControllers.js
+++ controllers/emailControllers.js
@@ -14,27 +14,25 @@ let transporter = nodemailer.createTransport({
 });
 
 const sendEmail = expressAsyncHandler(async (req, res) => {
-    const { email, subject, message } = req.body;
-   // console.log(email, subject, message);
-  
-    var mailOptions = {
-      from: email,
-      to: process.env.SMTP_MAIL,
-      subject: subject,
-      text: message,
-    };
-  
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        
-        return res.status(500).json({ message: "Failed to send email", error: error.message });
-      } else {
-        console.log("Email sent successfully!");
-       
-        return res.status(200).json({ message: "Email sent successfully!" });
-      }
-    });
+  const { email, subject, message } = req.body;
+
+  var mailOptions = {
+    from: process.env.SMTP_MAIL, // This should be the authenticated sender
+    to: process.env.SMTP_MAIL,   // You are sending the email to yourself
+    subject: subject,
+    text: message,
+    replyTo: email // This is the user's email, so you can reply to them
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Failed to send email", error: error.message });
+    } else {
+      console.log("Email sent successfully!");
+      return res.status(200).json({ message: "Email sent successfully!" });
+    }
   });
+});
 
 module.exports = { sendEmail };
